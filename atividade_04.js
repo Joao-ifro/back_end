3655c9fdf399a2412e944d985a52a52c3a70029b
Joao-ifro <joaovitorrhel11@gmail.com>
2025-05-22 17:42:29 -0400

    s

--- atividade_04.js
+++ atividade_04.js
@@ -2,4 +2,8 @@ import rl from 'readline-sync';
 
 //01. Escreva um programa que peça ao usuário para digitar um número e, em seguida, imprima a tabuada desse número usando um loop while.
 let num = rl.questionInt("informe um número: ");
-while ()
+let cont = 1;
+while ( cont <=10){
+    console.log(`${num} x ${cont} = ${num * cont}`);
+    cont++;
+}
